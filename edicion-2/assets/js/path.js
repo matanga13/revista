697e32c4bef89f9ac3e3da8e7d33487bbaf82c36
matanga13 <73069250+matanga13@users.noreply.github.com>
2022-11-09 18:58:09 -0300

    Update path.js

--- edicion-2/assets/js/path.js
+++ edicion-2/assets/js/path.js
@@ -1,2 +1,2 @@
-const PATH = window.location.protocol +"//"+ window.location.host+"/anclaje/edicion2/";
+const PATH = window.location.protocol +"//"+ window.location.host+"/anclaje/edicion-2/";
 document.head.innerHTML = document.head.innerHTML + "<base href="+PATH+" />";
